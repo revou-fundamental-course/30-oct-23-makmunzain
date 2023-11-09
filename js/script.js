1eed97cb51cdf10a7641cbab5e0c9f617e3421d4
Makmun Zain <makmunzain08@gmail.com>
2023-11-09 13:18:30 +0700

    add js

--- js/script.js
+++ js/script.js
@@ -11,19 +11,30 @@ function hitungBMI(){
     
     let kategoriBMI = document.getElementById("kategoriBMI");
     let keteranganBMI = document.getElementById("keteranganBMI");
+    let judulHasil = document.getElementById("judulHasil");
+    let rangeBMI = document.getElementById("rangeBMI");
+
+    judulHasil.textContent="Hasil";
+
+    let activeHasil = document.getElementById("hsl-bmi");
+    activeHasil.style.display = "flex";
 
     if (hasil >= 30){
         kategoriBMI.textContent="Kegemukan (Obesitas)";
         keteranganBMI.textContent="Anda mengalami kegemukan";
+        rangeBMI.textContent="Hasil BMI 30.0 atau lebih";
       } else if (hasil >= 25){
         kategoriBMI.textContent="Kelebihan berat badan";
         keteranganBMI.textContent="Anda memiliki berat badan berlebih";
+        rangeBMI.textContent="Hasil BMI diantara 25.0 dan 29.9";
       } else if (hasil >= 18.5){
         kategoriBMI.textContent="Normal (Ideal)";
         keteranganBMI.textContent="Anda memiliki berat badan normal";
+        rangeBMI.textContent="Hasil BMI diantara 18.5 dan 24.9";
       } else {
         kategoriBMI.textContent="Kekurangan berat badan";
         keteranganBMI.textContent="Anda mengalami kekurangan berat badan";
+        rangeBMI.textContent="Hasil BMI kurang dari 18.5";
       }
 }
 
@@ -42,6 +53,15 @@ function resetBMI(){
 
     let keteranganBMI = document.getElementById("keteranganBMI");
     keteranganBMI.textContent = "";
+
+    let judulHasil = document.getElementById("judulHasil");
+    judulHasil.textContent = "";
+
+    let rangeBMI = document.getElementById("rangeBMI");
+    rangeBMI.textContent = "";
+
+    let activeHasil = document.getElementById("hsl-bmi");
+    activeHasil.style.display = "none";
 
     pria.checked = pria.false;
     wanita.checked = wanita.false;
