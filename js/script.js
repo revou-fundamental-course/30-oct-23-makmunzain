891889eb82ec1375a1c1344269c9c133192b3453
Makmun Zain <makmunzain08@gmail.com>
2023-11-09 12:03:20 +0700

    add html and js

--- js/script.js
+++ js/script.js
@@ -10,15 +10,20 @@ function hitungBMI(){
     console.log(hasil)
     
     let kategoriBMI = document.getElementById("kategoriBMI");
+    let keteranganBMI = document.getElementById("keteranganBMI");
 
     if (hasil >= 30){
         kategoriBMI.textContent="Kegemukan (Obesitas)";
+        keteranganBMI.textContent="Anda mengalami kegemukan";
       } else if (hasil >= 25){
         kategoriBMI.textContent="Kelebihan berat badan";
+        keteranganBMI.textContent="Anda memiliki berat badan berlebih";
       } else if (hasil >= 18.5){
         kategoriBMI.textContent="Normal (Ideal)";
+        keteranganBMI.textContent="Anda memiliki berat badan normal";
       } else {
         kategoriBMI.textContent="Kekurangan berat badan";
+        keteranganBMI.textContent="Anda mengalami kekurangan berat badan";
       }
 }
 
@@ -34,6 +39,9 @@ function resetBMI(){
 
     let kategoriBMI = document.getElementById("kategoriBMI");
     kategoriBMI.textContent = "";
+
+    let keteranganBMI = document.getElementById("keteranganBMI");
+    keteranganBMI.textContent = "";
 
     pria.checked = pria.false;
     wanita.checked = wanita.false;
