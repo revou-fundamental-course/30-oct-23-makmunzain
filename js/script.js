f83ad5d4f005e0a14a58c60146c602cfb14295ad
Makmun Zain <makmunzain08@gmail.com>
2023-11-09 13:44:26 +0700

    add keterangan BMI

--- js/script.js
+++ js/script.js
@@ -13,6 +13,9 @@ function hitungBMI(){
     let keteranganBMI = document.getElementById("keteranganBMI");
     let judulHasil = document.getElementById("judulHasil");
     let rangeBMI = document.getElementById("rangeBMI");
+    let keterangan1 = document.getElementById("keterangan1");
+    let keterangan2 = document.getElementById("keterangan2");
+    let keterangan3 = document.getElementById("keterangan3");
 
     judulHasil.textContent="Hasil";
 
@@ -23,18 +26,30 @@ function hitungBMI(){
         kategoriBMI.textContent="Kegemukan (Obesitas)";
         keteranganBMI.textContent="Anda mengalami kegemukan";
         rangeBMI.textContent="Hasil BMI 30.0 atau lebih";
+        keterangan1.textContent = "Anda berada dalam kategori kegemukan atau obesitas."
+        keterangan2.textContent = "Cara terbaik untuk menurunkan berat badan adalah dengan mengatur kalori makanan yang dikonsumsi dan berolahraga."
+        keterangan3.textContent = "Jika BMI Anda berada dalam kategori ini maka Anda dianjurkan untuk menurunkan berat badan hingga batas normal."
       } else if (hasil >= 25){
         kategoriBMI.textContent="Kelebihan berat badan";
         keteranganBMI.textContent="Anda memiliki berat badan berlebih";
         rangeBMI.textContent="Hasil BMI diantara 25.0 dan 29.9";
+        keterangan1.textContent = "Anda berada dalam kategori overweight atau berat badan berlebih."
+        keterangan2.textContent = "Cara terbaik untuk menurunkan berat badan adalah dengan mengatur kalori makanan yang dikonsumsi dan berolahraga."
+        keterangan3.textContent = "Jika BMI Anda berada dalam kategori ini maka Anda dianjurkan untuk menurunkan berat badan hingga batas normal."
       } else if (hasil >= 18.5){
         kategoriBMI.textContent="Normal (Ideal)";
         keteranganBMI.textContent="Anda memiliki berat badan normal";
         rangeBMI.textContent="Hasil BMI diantara 18.5 dan 24.9";
+        keterangan1.textContent = "Anda berada dalam kategori normal atau ideal."
+        keterangan2.textContent = "Cara terbaik untuk menjaga berat badan agar tetap ideal adalah dengan mengatur kalori makanan yang dikonsumsi dan berolahraga."
+        keterangan3.textContent = "Jika BMI Anda berada dalam kategori ini maka Anda dianjurkan untuk menjaga berat badan agar tidak mencapai kategori berlebih maupun kurang."
       } else {
         kategoriBMI.textContent="Kekurangan berat badan";
         keteranganBMI.textContent="Anda mengalami kekurangan berat badan";
         rangeBMI.textContent="Hasil BMI kurang dari 18.5";
+        keterangan1.textContent = "Anda berada dalam kategori kekurangan berat badan."
+        keterangan2.textContent = "Cara terbaik untuk menaikan berat badan adalah dengan menambah jumlah kalori makanan yang dikonsumsi dan berolahraga."
+        keterangan3.textContent = "Jika BMI Anda berada dalam kategori ini maka Anda dianjurkan untuk menaikan berat badan hingga batas normal."
       }
 }
 
@@ -59,6 +74,15 @@ function resetBMI(){
 
     let rangeBMI = document.getElementById("rangeBMI");
     rangeBMI.textContent = "";
+
+    let keterangan1 = document.getElementById("keterangan1");
+    keterangan1.textContent = "";
+
+    let keterangan2 = document.getElementById("keterangan2");
+    keterangan2.textContent = "";
+
+    let keterangan3 = document.getElementById("keterangan3");
+    keterangan3.textContent = "";
 
     let activeHasil = document.getElementById("hsl-bmi");
     activeHasil.style.display = "none";
